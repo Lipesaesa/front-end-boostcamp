e894386a5c62357f58d49d5f5015f1d9d3bcd802
Felipe Gonzalez <felipetgonzalez07@gmail.com>
2025-08-12 00:11:20 -0300

    aprendi a conectar o react ao node usando api e axios

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Header from './components/Headers';
  */
 
 function App() {
-    const[projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web'])
+    const[projects, setProjects] = useState([])
 
     //useState retorna um array com 2 posições
     //
@@ -19,17 +19,24 @@ function App() {
     //2. Função para atualizarmos esse valor
 
     useEffect(() => {
-        api.get('/projects').then(response => {
-            console.log(useEffect)
-        });
-    }, []);
+    api.get('/projects').then(response => {
+        setProjects(response.data) // certo
+    });
+}, []);
 
-    function handleAddProject(){
+
+    async function handleAddProject(){
         // projects.push(`Novo projeto${Date.now()}`)
 
-        setProjects([ ...projects, `Novo projeto${Date.now()}`]);
+        // setProjects([ ...projects, `Novo projeto${Date.now()}`]);
+        const response = await api.post('projects', {
+            title: `Novo projeto ${Date.now()}`,
+            owner: "Felipe Gonzalez"
+        });
 
-        console.log(projects);
+        const project = response.data;
+
+        setProjects([...projects, project])
     }
 
     return (
@@ -38,7 +45,7 @@ function App() {
 
 
             <ul>
-                {projects.map(project => <li key={project}>{project}</li>)}
+                {projects.map(project => <li key={project.id}>{project.title}</li>)}
             </ul>
 
             <button type='button' onClick={handleAddProject}>Adicionar projeto</button>
